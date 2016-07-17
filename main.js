426607ef92cd581c931f69256cddba96beb76ac2
Sapir Chodorov <sapch8@gmail.com>
2016-07-17 23:50:43 +0300

    9st

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 
+function myFunction() {
+    document.getElementsByClassName("top")[0].classList.toggle("responsive");
+}
+
+
+
 var click = new Event ('myfolders');
 var click = new Event ('publicfolders');
 
@@ -8,16 +14,17 @@ function showExternalMy() {
     var site = document.getElementById("external-site-my");
     console.log('hi');
 
-    site.innerHTML = '<object type="text/html" data="http://www.one.co.il" ></object>';
+    site.innerHTML = '<object type="text/html" data="http://www.one.co.il" style="height: 90%";' +
+        'margin="2%"></object>';
 }
-
 
 function showExternalPublic() {
     console.log('hi1');
     var site = document.getElementById("external-site-public");
     console.log('hi1');
 
-    site.innerHTML = '<object type="text/html" data="http://www.sport5.co.il" ></object>';
+    site.innerHTML = '<object type="text/html" data="http://www.sport5.co.il" style="height: 90%";' +
+        'margin="2%"></object>';
 }
 
 myfolders.addEventListener('click', showExternalMy);
